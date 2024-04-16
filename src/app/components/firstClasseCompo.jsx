bae1f8f09e8c724867aa8d1f30198f88e9c33ac4
Vinybsqn <vianneybasquin59@gmail.com>
2024-04-16 11:05:05 +0200

    init/ creation first class/ first function
    components folder

---
+++ src/app/components/firstClasseCompo.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+export class FirstClasseComponent extends React.Component {
+    //  methode de rendu graphique
+    render(){
+        return (
+            <>
+                <h2>Mon composant (seule et unique !) Classe</h2>
+            </>
+        );
+    }
+};
+export default FirstClasseComponent;
---
+++ src/app/components/firstFunctionCompo.jsx
@@ -0,0 +1,19 @@
+export const FirstFunctionCompo = () => {
+    return (
+        <>
+            <h2>Mon composant fonction</h2>
+        </>
+    );
+}
+
+/*
+export function FirstFunctionCompo() {
+    return (
+        <>
+            <h2>Mon composant fonction</h2>
+        </>
+    );
+}
+ */
+
+export default FirstFunctionCompo;
